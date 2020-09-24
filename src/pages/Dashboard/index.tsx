2a06730257577691953176809f5c40506c101cbf
Tiago Soriano <tiago.ninjarj@gmail.com>
2020-09-24 00:00:56 -0400

    Add Schedule and Appointment itens to Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-import { FiPower } from "react-icons/fi";
-import { Container, Header, HeaderContent, Profile } from "./styles";
+import { FiClock, FiPower } from "react-icons/fi";
+import {
+	Container,
+	Header,
+	HeaderContent,
+	Profile,
+	Content,
+	Schedule,
+	NextAppointment,
+	Calendar,
+} from "./styles";
 
 import logoImg from "../../assets/logo.svg";
 import { useAuth } from "../../hooks/auth";
@@ -28,6 +37,34 @@ const Dashboard: React.FC = () => {
 					</button>
 				</HeaderContent>
 			</Header>
+
+			<Content>
+				<Schedule>
+					<h1>Horários agendados</h1>
+					<p>
+						<span>Hoje</span>
+						<span>Dia 06</span>
+						<span>Segunda-feira</span>
+					</p>
+
+					<NextAppointment>
+						<strong>Atendimento a seguir</strong>
+						<div>
+							<img
+								src="https://xesque.rocketseat.dev/users/avatar/profile-ae8dee25-12ab-4a55-b1b0-57e21e604ccd-1597033743541.jpg"
+								alt="Tiago Soriano"
+							/>
+
+							<strong>Tiago Soriano</strong>
+							<span>
+								<FiClock />
+								08:00
+							</span>
+						</div>
+					</NextAppointment>
+				</Schedule>
+				<Calendar />
+			</Content>
 		</Container>
 	);
 };
